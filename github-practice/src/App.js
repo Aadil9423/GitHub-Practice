f72f6c3417804988cbc867c4022a12970e2e9073
Aadil9423 <Aadilraza9763@gmail.com>
2022-02-21 16:52:44 +0530

    AD-raza

--- github-practice/src/App.js
+++ github-practice/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Aadil from "./Components/AADIL/Aadil";
 import Testing from "./Components/KHWAJA/Testing";
 function App() {
   return (
     <div className="App">
+      <h1>This is App.js</h1>
       <Testing />
+      <Aadil/>
     </div>
   );
 }
---
+++ github-practice/src/Components/AADIL/Aadil.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Aadil = () => {
+    return (
+        <div>
+            <h1>Hello I'M Aadil</h1>
+        </div>
+    );
+};
+
+export default Aadil;
